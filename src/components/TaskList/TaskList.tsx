a538348efa7ec5d4742c93266dbd88db76fdabbe
millan-figueroa <millan.fig@gmail.com>
2025-11-11 21:33:47 -0800

    Adds priority sorting (asc-dec) with dropdown using array copy.

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { TaskListProps } from "../../types";
 import TaskItem from "./TaskItem";
 
@@ -10,16 +11,55 @@ export default function TaskList({
     return <div>No tasks yet.</div>;
   }
 
+  //adds local state for sorting
+  const [sortOrder, setSortOrder] = useState<
+    "low" | "medium" | "high" | "none"
+  >("none");
+
+  // create temp array to sort using spread op to populate with elements from original tasks array
+  const sortedTasks = [...tasks];
+
+  // sorting logic
+  if (sortOrder !== "none") {
+    // defines sorting order
+    const priorities =
+      sortOrder === "low"
+        ? ["low", "medium", "high"]
+        : ["high", "medium", "low"];
+    // use indexOf to find the rank of each task's priority and compare them
+    sortedTasks.sort((a, b) => {
+      return priorities.indexOf(a.priority) - priorities.indexOf(b.priority);
+    });
+  }
+
   return (
-    <ul>
-      {tasks.map((task) => (
-        <TaskItem
-          key={task.id}
-          task={task}
-          onStatusChange={onStatusChange}
-          onDelete={onDelete}
-        />
-      ))}
-    </ul>
+    <>
+      {/* task sorting by priority*/}
+      <div className="flex justify-end mb-2">
+        <label className="text-sm text-gray-700 mr-2">Sort by Priority:</label>
+        <select
+          value={sortOrder}
+          onChange={(e) =>
+            setSortOrder(e.target.value as "low" | "high" | "none")
+          }
+          className="border border-gray-300 rounded px-2 py-1 text-sm"
+        >
+          <option value="none">None</option>
+          <option value="low">Low - High</option>
+          <option value="high">High - Low</option>
+        </select>
+      </div>
+
+      <ul>
+        {tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onStatusChange={onStatusChange}
+            onDelete={onDelete}
+          />
+        ))}
+      </ul>
+    </>
   );
 }
